a169492d41a41717ef19208208d4c28ebfeb07e3
Martin Kritzl <github@martin.kritzl.at>
2021-01-02 18:51:20 +0100

    added rotate FrontLeftDown; until stage 4

--- megaminx.pl
+++ megaminx.pl
@@ -35,7 +35,6 @@ mov(f,
 ).
 
 % rotate "right" slice
-% Todo: not finished
 mov(r,
     (
         U1,U2,U3,U4,U5,U6,U7,U8,U9,U10,U11, % Up
@@ -138,8 +137,8 @@ mov(u,
     )
 ).
 
-% rotate "FrontRideDown" slice
-mov(d,
+% rotate "FrontRightDown" slice
+mov(rd,
     cube(
         U1,U2,U3,U4,U5,U6,U7,U8,U9,U10,U11, % Up
         F1,F2,F3,F4,F5,F6,F7,F8,F9,F10,F11, % Front
@@ -167,6 +166,40 @@ mov(d,
         B1,B2,B3,B4,B5,B6,B7,B8,B9,B10,B11, % Back
         BLD1,BLD2,BLD3,BLD4,BLD5,BLD6,BLD7,BLD8,BLD9,BLD10,BLD11, % Back left down
         BRD1,BRD2,BRD3,R8,R9,R10,BRD7,BRD8,BRD9,BRD10,BRD11, % Back right down
+        BLU1,BLU2,BLU3,BLU4,BLU5,BLU6,BLU7,BLU8,BLU9,BLU10,BLU11, % Back left up
+        BRU1,BRU2,BRU3,BRU4,BRU5,BRU6,BRU7,BRU8,BRU9,BRU10,BRU11 % Back right up
+    )
+).
+
+% rotate "FrontLeftDown" slice
+mov(ld,
+    cube(
+        U1,U2,U3,U4,U5,U6,U7,U8,U9,U10,U11, % Up
+        F1,F2,F3,F4,F5,F6,F7,F8,F9,F10,F11, % Front
+        L1,L2,L3,L4,L5,L6,L7,L8,L9,L10,L11, % Left
+        R1,R2,R3,R4,R5,R6,R7,R8,R9,R10,R11, % Right
+        FLD1,FLD2,FLD3,FLD4,FLD5,FLD6,FLD7,FLD8,FLD9,FLD10,FLD11, % Front left down
+        FRD1,FRD2,FRD3,FRD4,FRD5,FRD6,FRD7,FRD8,FRD9,FRD10,FRD11, % Front right down
+
+        D1,D2,D3,D4,D5,D6,D7,D8,D9,D10,D11, % Down
+        B1,B2,B3,B4,B5,B6,B7,B8,B9,B10,B11, % Back
+        BLD1,BLD2,BLD3,BLD4,BLD5,BLD6,BLD7,BLD8,BLD9,BLD10,BLD11, % Back left down
+        BRD1,BRD2,BRD3,BRD4,BRD5,BRD6,BRD7,BRD8,BRD9,BRD10,BRD11, % Back right down
+        BLU1,BLU2,BLU3,BLU4,BLU5,BLU6,BLU7,BLU8,BLU9,BLU10,BLU11, % Back left up
+        BRU1,BRU2,BRU3,BRU4,BRU5,BRU6,BRU7,BRU8,BRU9,BRU10,BRU11 % Back right up
+    ),
+    cube(
+        U1,U2,U3,U4,U5,U6,U7,U8,U9,U10,U11, % Up
+        F1,F2,F3,F4,F5,L2,L3,L4,F9,F10,F11, % Front
+        L1,BLD6,BLD7,BLD8,L5,L6,L7,L8,L9,L10,L11, % Left
+        R1,R2,R3,R4,R5,R6,R7,R8,R9,R10,R11, % Right
+        FLD9,FLD10,FLD1,FLD2,FLD3,FLD4,FLD5,FLD6,FLD7,FLD8,FLD11, % Front left down
+        FRD1,FRD2,FRD3,FRD4,FRD5,FRD6,FRD7,F6,F7,F8,FRD11, % Front right down
+
+        D1,D2,D3,FRD8,FRD9,FRD10,D7,D8,D9,D10,D11, % Down
+        B1,B2,B3,B4,B5,B6,B7,B8,B9,B10,B11, % Back
+        BLD1,BLD2,BLD3,BLD4,BLD5,D4,D5,D6,BLD9,BLD10,BLD11, % Back left down
+        BRD1,BRD2,BRD3,BRD4,BRD5,BRD6,BRD7,BRD8,BRD9,BRD10,BRD11, % Back right down
         BLU1,BLU2,BLU3,BLU4,BLU5,BLU6,BLU7,BLU8,BLU9,BLU10,BLU11, % Back left up
         BRU1,BRU2,BRU3,BRU4,BRU5,BRU6,BRU7,BRU8,BRU9,BRU10,BRU11 % Back right up
     )
@@ -247,9 +280,9 @@ mov(v,
 % --------------------------------------------------------------
 %F2L
 mov(sp1,X,Y) :-
-    move_sequence([+u, +r, -u, -r, -u, -f, +u, +f], X, Y).
+    move_sequence([+f, +rd, -f, -rd, -f, -ld, +f, +ld], X, Y).
 mov(sp2,X,Y) :-
-    move_sequence([-u, -l, +u, +l, +u, +f, -u, -f], X, Y).
+    move_sequence([-f, -ld, +f, +ld, +f, +rd, -f, -rd], X, Y).
 
 %Third layer edges and corners
 mov(sp3,X,Y) :-
@@ -277,7 +310,7 @@ mov(sp10,X,Y) :-
 
 %Orient corners
 mov(sp11,X,Y) :-
-    move_sequence([-r, -d, +r, +d], X, Y).  
+    move_sequence([-r, -rd, +r, +rd], X, Y).  
 
 % --------------------------------------------------------------
 % these helpers call the actual rotations
@@ -342,22 +375,40 @@ get_goal(2, cube(
      _ , _ , _ , _ , _ ,'BRU','BRU','BRU', _ , _ , _ ) % Back right up / dunkel blau
     ).
 
-% Turn cube
+% Turn cube upside-down
 get_goal(3, cube(
     %1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10, 11
-     _ , _ , _ , _ , _ , _ , _ , _ , _ , _ , _ , % Up / weiss
-     _ , _ , _ , _ , _ , _ , _ , _ , _ , _ , _ , % Front / dunkel gruen
-     _ , _ , _ , _ , _ , _ , _ , _ , _ , _ , _ , % Left / violett
-     _ , _ , _ , _ , _ , _ , _ , _ , _ , _ , _ , % Right / rot
-     _ , _ , _ ,'F','F','F', _ , _ , _ , _ , _ , % Front left down / hell blau
-     _ , _ , _ , _ , _ ,'L','L','L', _ , _ , _ , % Front right down / braun
-    'U','U','U','U','U','U','U','U','U','U','U', % Down / grau
-    'BRU','BRU', _ , _ , _ , _ , _ , _ , _ ,'BRU', _ , % Back / hell gruen
-     _ , _ , _ , _ , _ , _ , _ ,'R','R','R', _ , % Back left down / orange
-     _ , _ , _ , _ , _ , _ , _ , _ , _ , _ , _ , % Back right down / rosa
-     _ , _ , _ , _ , _ , _ , _ , _ , _ , _ , _ , % Back left up / gelb
-     _ , _ , _ , _ , _ ,'BRU','BRU','BRU', _ , _ , _ ) % Back right up / dunkel blau
+     _ , _ , _ , _ , _ , _ , _ , _ , _ , _ , _ , % Up / grau
+     _ , _ , _ , _ , _ , _ , _ , _ , _ , _ , _ , % Front / hellblau
+     _ , _ , _ , _ , _ , _ , _ , _ , _ , _ , _ , % Left / braun
+     _ , _ , _ , _ , _ , _ , _ , _ , _ , _ , _ , % Right / orange
+     _ , _ , _ ,'F','F','F', _ , _ , _ , _ , _ , % Front left down / dunkelgruen
+     _ , _ , _ , _ , _ ,'L','L','L', _ , _ , _ , % Front right down / violett
+    'U','U','U','U','U','U','U','U','U','U','U', % Down / weiss
+    'BRU','BRU', _ , _ , _ , _ , _ , _ , _ ,'BRU', _ , % Back / dunkelblau
+     _ , _ , _ , _ , _ , _ , _ ,'R','R','R', _ , % Back left down / rot
+     _ ,'BLU','BLU','BLU', _ , _ , _ , _ , _ , _ , _ , % Back right down / gelb
+     _ , _ , _ , _ , _ , _ , _ , _ , _ , _ , _ , % Back left up / rosa
+     _ , _ , _ , _ , _ , _ , _ , _ , _ , _ , _ ) % Back right up / hellgruen
     ).
+
+% edge pieces 2nd layer
+get_goal(4, cube(
+    %1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10, 11
+     _ , _ , _ , _ , _ , _ , _ , _ , _ , _ , _ , % Up / grau
+     _ , _ , _ , _ , _ , _ , _ , _ , _ , _ , _ , % Front / hellblau
+     _ , _ , _ , _ , _ , _ , _ , _ , _ , _ , _ , % Left / braun
+     _ , _ , _ , _ , _ , _ , _ , _ , _ , _ , _ , % Right / orange
+     _ , _ ,'F','F','F','F','F', _ , _ , _ , _ , % Front left down / dunkelgruen
+     _ , _ , _ , _ ,'L','L','L','L','L', _ , _ , % Front right down / violett
+    'U','U','U','U','U','U','U','U','U','U','U', % Down / weiss
+    'BRU','BRU','BRU', _ , _ , _ , _ , _ ,'BRU','BRU', _ , % Back / dunkelblau
+     'R', _ , _ , _ , _ , _ ,'R','R','R','R', _ , % Back left down / rot
+     'BLU','BLU','BLU','BLU','BLU', _ , _ , _ , _ , _ , _ , % Back right down / gelb
+     _ , _ , _ , _ , _ , _ , _ , _ , _ , _ , _ , % Back left up / rosa
+     _ , _ , _ , _ , _ , _ , _ , _ , _ , _ , _ ) % Back right up / hellgruen
+    ).
+
 
 get_goal(1, cube(
     _,'F',_,_,_,_,_,_,_,
